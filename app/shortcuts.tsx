f476019022ed5b4cffceac12e3287dc8143e4eef
Roohi Mathur <roohimathur@Roohis-MacBook-Pro.local>
2025-12-16 13:33:28 +0000

    uncoomented code in shortcuts

--- app/shortcuts.tsx
+++ app/shortcuts.tsx
@@ -4,9 +4,9 @@ import { Ionicons } from "@expo/vector-icons";
 export default function ShortcutsScreen() {
   return (
     <View className="flex-1 bg-gray-100 p-4">
-      {/* <Text className="text-2xl font-bold mb-4">Shortcuts</Text> */}
- <Text className="text-5xl font-bold text-white">Hello</Text>
-      {/* <View className="flex-row flex-wrap justify-between">
+      <Text className="text-2xl font-bold mb-4">Shortcuts</Text>
+ 
+      <View className="flex-row flex-wrap justify-between">
         {[
           { title: "Orders", icon: "receipt-outline" },
           { title: "Wishlist", icon: "heart-outline" },
@@ -21,7 +21,7 @@ export default function ShortcutsScreen() {
             <Text className="mt-2 font-semibold">{item.title}</Text>
           </Pressable>
         ))}
-      </View> */}
+      </View>
     </View>
   );
 }
